8e227e219e5de588939dcdba200f252d6d533728
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-12-06 23:57:25 -0600

    added cross-validation and helper functions for evaluation

--- DT_classifier.py
+++ DT_classifier.py
@@ -1,6 +1,7 @@
 from random import seed
 from random import randrange
 from csv import reader
+import 
 from __future__ import print_function
 
 
@@ -175,25 +176,83 @@ def print_leaf(counts):
     return probs
 
 
+def decision_tree(training_data, testing_data):
+    decision_tree = build_tree(training_data)
+    predictions = list()
+    for row in testing_data:
+        prediction = print_leaf(predict(row, decision_tree))
+        predictions.append(prediction)
+    return(predictions)
+
+def cross_validation_split(dataset, n_folds):
+    """
+    This function helps split a dataset into k folds
+    Standard function cited from www.machinelearningmastery.com
+    """
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+def accuracy_metric(actual, predicted):
+    """
+    This function helps calculate the accuracy percentage
+    Standard function cited from www.machinelearningmastery.com
+    """
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    """
+    This function helps to evaluate an algorithm using a cross validation split
+    Standard function cited from www.machinelearningmastery.com
+    """
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
 
 
 if __name__ == '__main__':
 
+    # Dataset
     training_data = [
-    ['Green', 3, 'Apple'],
-    ['Yellow', 3, 'Apple'],
-    ['Red', 1, 'Grape'],
-    ['Red', 1, 'Grape'],
-    ['Yellow', 3, 'Lemon'],
+        ['Green', 3, 'Apple'],
+        ['Yellow', 3, 'Apple'],
+        ['Red', 1, 'Grape'],
+        ['Red', 1, 'Grape'],
+        ['Yellow', 3, 'Lemon'],
     ]
 
-    # Column labels.
-    # These are used only to print the tree.
     header = ["color", "diameter", "label"]
 
-    my_tree = build_tree(training_data)
-
-    print_tree(my_tree)
+    # evaluate algorithm
+    n_folds = 5
+    scores = evaluate_algorithm(dataset, decision_tree, n_folds)
+    print('Scores: %s' % scores)
+    print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
 
     # Evaluate
     testing_data = [
@@ -206,4 +265,4 @@ if __name__ == '__main__':
 
     for row in testing_data:
         print ("Actual: %s. Predicted: %s" %
-               (row[-1], print_leaf(classify(row, my_tree))))
+               (row[-1], print_leaf(predict(row, decision_tree))))
